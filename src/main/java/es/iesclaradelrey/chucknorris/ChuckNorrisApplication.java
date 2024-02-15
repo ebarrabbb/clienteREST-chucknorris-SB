25470401c7588f8630e36084aaeaf5c37251ad62
Eduardo Barra Balao <ebarrab@gmail.com>
2024-02-16 00:08:59 +0100

    0.1 Spring Boot. Petición REST a https://api.chucknorris.io/jokes/random [texto plano]

---
+++ src/main/java/es/iesclaradelrey/chucknorris/ChuckNorrisApplication.java
@@ -0,0 +1,17 @@
+package es.iesclaradelrey.chucknorris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ChuckNorrisApplication
+ * @version 0.1
+ * @author Eduardo Barra Balao
+ */
+
+@SpringBootApplication
+public class ChuckNorrisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisApplication.class, args);
+    }
+}
---
+++ src/main/java/es/iesclaradelrey/chucknorris/ClienteREST.java
@@ -0,0 +1,18 @@
+package es.iesclaradelrey.chucknorris;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ClienteREST {
+    @GetMapping("/chuck")
+        public String obtenerChuck() {
+        String url = "https://api.chucknorris.io/jokes/random";    // Cambia la URL según tu API REST
+
+        RestTemplate restTemplate = new RestTemplate();
+        String respuesta = restTemplate.getForObject(url, String.class);
+
+        return respuesta;
+    }
+}
